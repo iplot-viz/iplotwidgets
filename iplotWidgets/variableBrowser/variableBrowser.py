f2b05100ecb224775f69b01625978f5e01f6cf5f
cabanij <jscabanilla@minsait.com>
2024-01-10 11:31:58 +0000

    IDV-476 New classes needed

--- iplotWidgets/variableBrowser/variableBrowser.py
+++ iplotWidgets/variableBrowser/variableBrowser.py
@@ -1,13 +1,15 @@
 import time
 
 from PySide6.QtGui import QGuiApplication
-from PySide6.QtWidgets import QWidget, QStyle, QLineEdit, QPushButton, QComboBox, QHBoxLayout, QVBoxLayout, QProgressBar
+from PySide6.QtWidgets import QWidget, QStyle, QLineEdit, QPushButton, QComboBox, QHBoxLayout, QVBoxLayout, QProgressBar, QMessageBox
 from PySide6.QtCore import Qt, Signal
 from iplotDataAccess.appDataAccess import AppDataAccess
 from iplotWidgets.variableBrowser.variableTree import VariableTree
-from iplotWidgets.variableBrowser.variableTable import VariableTable
+from iplotWidgets.variableBrowser.variableTable import VariableTable, ModuleTable
 from iplotWidgets.variableBrowser.tools.converters import parse
 from iplotLogging import setupLogger as setupLog
+from iplotDataAccess.appDataAccess import AppDataAccess
+from iplotProcessing.tools.parsers import Parser
 
 logger = setupLog.get_logger(__name__)
 
@@ -215,3 +217,112 @@ class VariableBrowser(QWidget):
             time.sleep(3)
             self.progress_bar.setStyleSheet("")
             self.progress_bar.hide()
+
+class ModuleBrowser(QWidget):
+    cmd_finish = Signal(object)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.resize(500, 400)
+        self.width = 840
+        self.height = 680
+        self.setAcceptDrops(True)
+        self.setGeometry(
+            QStyle.alignedRect(
+                Qt.LeftToRight,
+                Qt.AlignCenter,
+                self.size(),
+                QGuiApplication.primaryScreen().availableGeometry(),
+            ),
+        )
+        self.parser = Parser()
+
+        self.tableView = ModuleTable()
+        self.searchbar = QLineEdit()
+
+        self.path_input = QLineEdit()
+        self.clear_module = QPushButton('Clear selected module')
+        self.clear_module.clicked.connect(self.clear_selected_module)
+        self.clear_all_module = QPushButton('Clear all modules')
+        self.clear_all_module.clicked.connect(self.reset_modules)
+
+        self.search_btn = QPushButton('Add')
+        self.search_btn.clicked.connect(self.check_module)
+
+        top_h_layout = QHBoxLayout()
+        top_h_layout.addWidget(self.searchbar)
+        top_h_layout.addWidget(self.search_btn)
+        top_v_layout = QVBoxLayout()
+        top_v_layout.addLayout(top_h_layout)
+
+        bot_h_layout = QHBoxLayout()
+        bot_h_layout.addWidget(self.clear_module)
+        bot_h_layout.addWidget(self.clear_all_module)
+
+        mid_h_layout = QHBoxLayout()
+        mid_h_layout.addWidget(self.tableView)
+        main_v_layout = QVBoxLayout()
+        main_v_layout.addLayout(top_v_layout)
+        self.add_layout = main_v_layout.addLayout(mid_h_layout)
+        main_v_layout.addLayout(bot_h_layout)
+        self.setLayout(main_v_layout)
+
+        #self.finish_btn.clicked.connect(self.finish)
+
+        # Show available modules when loading the window.
+        self.starter_modules()
+
+    def starter_modules(self):
+        modules = self.get_current_modules()
+
+        data_list = self.tableView.get_variables_list()
+        for module in modules:
+            self.tableView.model.add_row([module])
+
+    def get_current_modules(self):
+        return self.parser.get_modules()
+
+    def check_module(self):
+        text = self.searchbar.text()
+        if text == '':
+            return
+
+        try:
+            self.parser.load_modules(text)
+
+        except Exception as e:
+            box = QMessageBox()
+            box.setIcon(QMessageBox.Critical)
+            box.setText(
+                f"Error {str(e)}: cannot import module")
+            logger.exception(e)
+            box.exec_()
+            return
+
+        # If the module was loaded correctly, it will be shown in the module table.
+        modules = self.parser.get_modules()
+        data_list = self.tableView.get_variables_list()
+        for value in modules:
+            if value not in data_list:
+                self.tableView.model.add_row([value])
+
+    def clear_selected_module(self):
+        rows = self.tableView.remove_selected_module()
+        rows = [i for i in rows if i > 3]
+        self.parser.clear_modules(rows)
+
+    def reset_modules(self):
+        self.parser.reset_modules()
+        self.tableView.clear_table()
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key.Key_Return:
+            self.add_to_table()
+        elif event.key() == Qt.Key.Key_Delete:
+            self.tableView.remove_from_table()
+
+    def finish(self):
+        df = self.tableView.get_variables_df()
+        self.cmd_finish.emit(df)
+        self.tableView.clear_table()
--- iplotWidgets/variableBrowser/variableTable.py
+++ iplotWidgets/variableBrowser/variableTable.py
@@ -82,3 +82,94 @@ class VariableTable(QTableView):
 
     def get_variables_list(self):
         return self.model.get_model_list()
+
+
+class ModuleTableModel(QAbstractTableModel):
+
+    def __init__(self, ):
+        super(ModuleTableModel, self).__init__()
+        self._dataframe = pd.DataFrame(columns=['Module'])
+
+    @property
+    def dataframe(self) -> pd.DataFrame:
+        """Return the data"""
+        return self._dataframe
+
+    @dataframe.setter
+    def dataframe(self, dataframe: pd.DataFrame):
+        """Set path of the current item"""
+        self._dataframe = dataframe
+
+    def data(self, index: Union[QModelIndex, QPersistentModelIndex], role: int = ...) -> Any:
+        if role == Qt.ItemDataRole.DisplayRole:
+            value = self.dataframe.iloc[index.row(), index.column()]
+            return str(value)
+
+    def rowCount(self, parent: Union[QModelIndex, QPersistentModelIndex] = ...) -> int:
+        return self.dataframe.shape[0]
+
+    def columnCount(self, parent: Union[QModelIndex, QPersistentModelIndex] = ...) -> int:
+        return 1
+
+    def headerData(self, section: int, orientation: Qt.Orientation, role: int = ...) -> Any:
+        if role == Qt.ItemDataRole.DisplayRole:
+            if orientation == Qt.Orientation.Horizontal:
+                return str(self.dataframe.columns[section])
+
+    def add_row(self, new_values: List):
+        new_dataframe = pd.DataFrame([new_values], columns=['Module'])
+        self.dataframe = pd.concat([self.dataframe, new_dataframe]).reset_index(drop=True)
+        self.layoutChanged.emit()
+
+    def remove_row(self, selectedModule):
+        # Check
+        selectedModule = [i for i in selectedModule if i > 3]
+        self.dataframe.drop(selectedModule, inplace=True)
+        self.dataframe.reset_index(drop=True, inplace=True)
+        self.layoutChanged.emit()
+
+    def clear_model(self):
+        self.dataframe = self.dataframe.head(4)
+        self.layoutChanged.emit()
+
+    def get_model_list(self):
+        return self.dataframe['Module'].values.tolist()
+
+class ModuleTable(QTableView):
+    def __init__(self):
+        QTableView.__init__(self)
+        self.setSelectionMode(self.selectionMode().ExtendedSelection)
+        self.model_table = QStandardItemModel()
+        self.setModel(self.model_table)
+        self.verticalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Fixed)
+        self.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Fixed)
+        self.horizontalHeader().setStretchLastSection(True)
+        self.setSelectionBehavior(QAbstractItemView.SelectionBehavior.SelectRows)
+        self.setColumnWidth(0, 100)
+        self.model = ModuleTableModel()
+        self.setModel(self.model)
+
+        print()
+
+    def remove_from_table(self):
+        index = self.selectedIndexes()
+        rows = [ix.row() for ix in index]
+        for row in reversed(rows):
+            self.model_table.removeRow(row)
+        self.clearSelection()
+
+    def remove_selected_module(self):
+        index = self.selectedIndexes()
+        rows = [ix.row() for ix in index]
+        self.model.remove_row(rows)
+        self.clearSelection()
+        return rows
+
+    def clear_table(self):
+        self.model.clear_model()
+
+    def get_variables_df(self) -> pd.DataFrame:
+        return self.model.dataframe
+
+    def get_variables_list(self):
+        return self.model.get_model_list()
