bacb8f3696e1350163293f3886c2113eeacef0a8
cabanij <jscabanilla@minsait.com>
2024-06-07 12:53:21 +0000

    IDV-309 A different model has been created depending on the data source to improve efficiency and performance.

--- iplotWidgets/pulseBrowser/PulseTable.py
+++ iplotWidgets/pulseBrowser/PulseTable.py
@@ -1,6 +1,5 @@
 from PySide6.QtCore import Qt
-from PySide6.QtGui import QCursor
-from PySide6.QtWidgets import QTableView, QAbstractItemView, QHeaderView, QToolTip
+from PySide6.QtWidgets import QTableView, QAbstractItemView, QHeaderView
 
 from iplotDataAccess.appDataAccess import AppDataAccess
 from iplotWidgets.pulseBrowser.models.PulseTableModel import PulseTableModel
@@ -16,43 +15,39 @@ class PulseTable(QTableView):
         self.setSelectionBehavior(QAbstractItemView.SelectionBehavior.SelectRows)
         self.setColumnWidth(0, 100)
 
-        self.model = PulseTableModel()
+        # self.model = PulseTableModel()
+        self.models = {'SEARCH': PulseTableModel(data_source=AppDataAccess.da.defaultds)}
+        self.current_model = ''
+
         self.page_size = 20  # pulses per page
         self.page_num = 1  # current page
 
         self.setContextMenuPolicy(Qt.CustomContextMenu)
-
         self.setMouseTracking(True)
-
-        self.entered.connect(self.handle_item_entered)
         self.setAlternatingRowColors(True)
 
         self.load_model(AppDataAccess.da.defaultds)
 
+        self.adjust_columns(AppDataAccess.da.defaultds)
+
+    def adjust_columns(self, data_source):
+        # Adjust
+        for column in range(self.models[data_source.name].dataframe.shape[1]):
+            self.resizeColumnToContents(column)
+
     def load_model(self, data_source):
-        self.model.load(data_source, self.page_size, self.page_num)
-        self.setModel(self.model)
+        ds_name = data_source.name
+        if ds_name not in self.models:
+            self.models[ds_name] = PulseTableModel(data_source=data_source)
+            self.models[ds_name].load(self.page_size, self.page_num)
 
-    def set_model(self):
-        self.setModel(self.model)
+        self.current_model = ds_name
+        self.setModel(self.models[ds_name])
 
-    def handle_item_entered(self, index):
-        if not index.isValid():
-            return
-        pulse_index = index.row()
-        ix = self.model.get_pulse(pulse_index)
-
-        QToolTip.showText(
-            QCursor.pos(),
-            f'{ix.key}\n'
-            f'Description: {ix.description}\n'
-            f'Status: {ix.status}\n'
-            f'Time From: {ix.timeFrom}\n'
-            f'Time To: {ix.timeTo}\n'
-            f'Duration: {ix.duration}\n',
-            self.viewport(),
-            self.visualRect(index)
-        )
+    def set_model(self, data_source_name):
+        if data_source_name in self.models:
+            self.current_model = data_source_name
+            self.setModel(self.models[data_source_name])
 
     def reset_page(self, found: bool = True):
         if found:
