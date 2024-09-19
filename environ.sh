c5599d8066aeaa21bf9a76312df003920edb6600
Paulo Abreu <paulo.abreu@iter.org>
2024-09-19 10:58:22 +0200

    CI: loading modules from a clean state

--- environ.sh
+++ environ.sh
@@ -16,7 +16,6 @@ function try ()
   "$@" || die "cannot $*" 
 }
 
-
 # Default to foss toolchain
 if [[ "$1" == "foss" || -z $1 ]];
 then
@@ -26,10 +25,25 @@ then
     toolchain=intel
 fi
 echo "Toolchain: $toolchain"
+# Clean slate
+try module purge
 
 # Testing/Coverage requirements
-try module load coverage/5.5-GCCcore-10.2.0 
-
+case $toolchain in
+  "foss")
+      try module load IMAS-AL-Python/5.3.0-foss-2023b-DD-3.42.0
+      try module load coverage/7.4.4-GCCcore-13.2.0
+    ;;
+  "intel")
+      try module load IMAS-AL-Python/5.3.0-intel-2023b-DD-3.42.0
+      # only in foss, but it works anyway:
+      try module load coverage/7.4.4-GCCcore-13.2.0
+    ;;
+   *)
+    echo "Unknown toolchain $toolchain"
+    ;;
+esac
+try module -t list 2>&1 | sort
 
 export HOME=$PWD
 echo "HOME was set to $HOME"
