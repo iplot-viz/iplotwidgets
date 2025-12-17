9b300c389ef6bf05514fd56f3a02cac20dcc7ced
Jhon <jscabanilla@minsait.com>
2025-12-17 16:58:19 +0000

    IDV-732 Migrate project configuration to pyproject.toml. - Remove requirements.txt (dependencies now managed via pyproject.toml)

--- setup.py
+++ setup.py
@@ -1,39 +1,7 @@
 import setuptools
 import versioneer
 
-# from iplotLogging._version import __version__
-
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
-    name="iplotWidgets",
     version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    setup_requires=["setuptools-git-versioning"],
-    # version_config={
-    #     "version_callback": __version__,
-    #     "template": "{tag}",
-    #     "dirty_template": "{tag}.dev{ccount}.{sha}",
-    # },
-    author="Lana Abadie",
-    author_email="lana.abadie@iter.org",
-    description="IDV widget components library",
-    long_description=long_description,
-    url="https://github.com/iplot-viz/iplotwidgets.git",
-    project_urls={
-        "Bug Tracker": "https://jira.iter.org/issues/?jql=project+%3D+IDV+AND+component+%3D+iplotwidgets",
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        # "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    package_dir={"": "."},
-    packages=setuptools.find_namespace_packages(where="."),
-    python_requires=">=3.8",
-    install_requires=[
-        "iplotLogging",
-        "iplotDataAccess",
-    ],
+    cmdclass=versioneer.get_cmdclass()
 )
